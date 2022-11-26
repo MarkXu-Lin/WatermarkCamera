619730df8653824c64ef6f39d75f6fd98a58a5ad
mark1970 <1120904432@qq.com>
2022-11-26 17:49:59 +0800

    编写拍摄视频相关代码

--- app/src/main/java/com/mark/watermarkcamera/activity/CameraActivity.kt
+++ app/src/main/java/com/mark/watermarkcamera/activity/CameraActivity.kt
@@ -1,10 +1,25 @@
 package com.mark.watermarkcamera.activity
 
+import android.app.RecoverableSecurityException
+import android.content.ContentUris
+import android.content.ContentValues
+import android.content.IntentSender
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
 import android.hardware.Camera
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.os.ParcelFileDescriptor
+import android.provider.MediaStore
+import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO
 import android.util.Log
+import android.widget.ImageView
 import androidx.viewbinding.ViewBinding
 import com.mark.watermarkcamera.base.BaseActivity
 import com.mark.watermarkcamera.widget.camera.CameraPreview
@@ -14,6 +29,7 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.jar.Manifest
@@ -25,8 +41,6 @@ abstract class CameraActivity<T: ViewBinding> : BaseActivity<T>() {
     private var mCamera: Camera? = null
     private var mPreview: CameraPreview? = null
 
-    private val MEDIA_TYPE_IMAGE = 1
-    private val MEDIA_TYPE_VIDEO = 2
 
     private val mPicture = Camera.PictureCallback{data, _ ->
         val pictureFile: File = getOutputMediaFile(MEDIA_TYPE_IMAGE) ?: run {
@@ -78,6 +92,58 @@ abstract class CameraActivity<T: ViewBinding> : BaseActivity<T>() {
         }
     }
 
+    private fun saveImgFileWithMediaStore(){
+        val imageMediaPath = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Demo").path
+        }else{
+            Environment.DIRECTORY_PICTURES + "/Demo"
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            try {
+                val bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888)
+                //创建了一个红色的图片
+                val canvas = Canvas(bitmap)
+                canvas.drawColor(Color.RED)
+                val fos = FileOutputStream(imageMediaPath)
+                bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos)
+                fos.close()
+            }catch (e: FileNotFoundException){
+                Log.d(TAG, "创建失败：${e.message}")
+            }catch (e: IOException){
+                Log.d(TAG, "创建失败：${e.message}")
+            }
+        }else{
+            val values = ContentValues()
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, "red_image.png")
+            values.put(MediaStore.Images.Media.DESCRIPTION, "This is a Image")
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, imageMediaPath)
+            val external = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            val insertUri = contentResolver.insert(external, values)
+            var os: OutputStream? = null
+            try {
+                if (insertUri != null){
+                    os = contentResolver.openOutputStream(insertUri)
+                }
+                if (os != null){
+                    val bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888)
+                    //创建了一个红色的图片
+                    val canvas = Canvas(bitmap)
+                    canvas.drawColor(Color.RED)
+                    // 向os流写入数据
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, os)
+                }
+            }catch (e: IOException){
+                Log.d(TAG, "创建失败：${e.message}")
+            }finally {
+                os?.close()
+            }
+        }
+    }
+
+    private fun deleteFile(){
+    }
+
     abstract fun initPreview(preview: CameraPreview?)
 
     @NeedsPermission(android.Manifest.permission.CAMERA)
@@ -106,5 +172,10 @@ abstract class CameraActivity<T: ViewBinding> : BaseActivity<T>() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         // NOTE: delegate the permission handling to generated method
         onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    companion object{
+        const val MEDIA_TYPE_IMAGE = 1
+        const val MEDIA_TYPE_VIDEO = 2
     }
 }
--- app/src/main/java/com/mark/watermarkcamera/widget/camera/CameraPreview.kt
+++ app/src/main/java/com/mark/watermarkcamera/widget/camera/CameraPreview.kt
@@ -2,13 +2,23 @@ package com.mark.watermarkcamera.widget.camera
 
 import android.content.Context
 import android.hardware.Camera
+import android.media.CamcorderProfile
+import android.media.MediaRecorder
+import android.os.Environment
+import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO
 import android.util.Log
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import com.mark.watermarkcamera.activity.CameraActivity
+import java.io.File
 import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraPreview(context: Context, private val mCamera: Camera): SurfaceView(context), SurfaceHolder.Callback {
     private val TAG = "Mark007"
+    private lateinit var mediaRecorder: MediaRecorder
     private val mHolder: SurfaceHolder = holder.apply {
         addCallback(this@CameraPreview)
         // deprecated setting, but required on Android versions prior to 3.0
@@ -16,14 +26,7 @@ class CameraPreview(context: Context, private val mCamera: Camera): SurfaceView(
     }
 
     override fun surfaceCreated(holder: SurfaceHolder) {
-        mCamera.apply {
-            try {
-                setPreviewDisplay(holder)
-                startPreview()
-            }catch (e: IOException){
-                Log.d(TAG, "Error setting camera preview: ${e.message}")
-            }
-        }
+        startPreview()
     }
 
     override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
@@ -35,6 +38,14 @@ class CameraPreview(context: Context, private val mCamera: Camera): SurfaceView(
         }catch (e: Exception){
             Log.d(TAG, "Error surfaceChanged: ${e.message}")
         }
+        startPreview()
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    private fun startPreview(){
         mCamera.apply {
             try {
                 setPreviewDisplay(mHolder)
@@ -45,7 +56,70 @@ class CameraPreview(context: Context, private val mCamera: Camera): SurfaceView(
         }
     }
 
-    override fun surfaceDestroyed(holder: SurfaceHolder) {
-        // empty. Take care of releasing the Camera preview in your activity.
+    private fun prepareVideoRecorder(): Boolean{
+        mediaRecorder = MediaRecorder()
+        mCamera.let {
+            // Step 1: Unlock and set camera to MediaRecorder
+            it.unlock()
+            mediaRecorder.run {
+                setCamera(it)
+                setAudioSource(MediaRecorder.AudioSource.CAMCORDER)
+                setVideoSource(MediaRecorder.VideoSource.CAMERA)
+                setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH))
+                setOutputFile(getOutputMediaFile(CameraActivity.MEDIA_TYPE_VIDEO).toString())
+                setPreviewDisplay(mHolder.surface)
+                setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+                setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT)
+                // Step 6: Prepare configured MediaRecorder
+                return try {
+                    prepare()
+                    true
+                } catch (e: IllegalStateException) {
+                    Log.d(TAG, "IllegalStateException preparing MediaRecorder: ${e.message}")
+                    releaseMediaRecorder()
+                    false
+                } catch (e: IOException) {
+                    Log.d(TAG, "IOException preparing MediaRecorder: ${e.message}")
+                    releaseMediaRecorder()
+                    false
+                }
+            }
+        }
     }
+
+    /** Create a File for saving an image or video */
+    private fun getOutputMediaFile(type: Int): File?{
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        val mediaStorageDir = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "MyCameraApp")
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+        // Create the storage directory if it does not exist
+        mediaStorageDir.apply {
+            if (!exists()){
+                if (!mkdirs()){
+                    Log.d(TAG, "failed to create directory")
+                    return null
+                }
+            }
+        }
+        // Create a media file name
+        val timeStamp  = SimpleDateFormat("yyyyMMdd_Hhmmss", Locale.getDefault()).format(Date())
+        return when(type){
+            MEDIA_TYPE_IMAGE -> {
+                File("${mediaStorageDir.path}${File.separator}IMG_$timeStamp.jpg")
+            }
+            MEDIA_TYPE_VIDEO -> {
+                File("${mediaStorageDir.path}${File.separator}VID_$timeStamp.mp4")
+            }
+            else -> null
+        }
+    }
+    private fun releaseMediaRecorder() {
+        mediaRecorder.reset() // clear recorder configuration
+        mediaRecorder.release() // release the recorder object
+        mCamera.lock() // lock camera for later use
+    }
+
 }
